47137ac7f0f43898a1fd20fcb9ada840bfb5e5cf
Warze <contact@warze.org>
2023-10-22 13:51:43 +0200

    First steps

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 import './css/main.css';
+import './css/nav.css';
+import './css/page.css';
+import './css/footer.css';
+
+document.querySelectorAll<HTMLElement>('.lorem').forEach((element) => {
+	element.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+});
