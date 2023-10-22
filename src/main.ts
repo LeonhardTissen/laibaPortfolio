9cf3f8c4481ca8ee4ff294a4c8a2dcb41ec67321
Warze <contact@warze.org>
2023-10-22 14:54:55 +0200

    Applied footer navigation & reach out logos

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,10 @@ import './css/footer.css';
 document.querySelectorAll<HTMLElement>('.lorem').forEach((element) => {
 	element.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 });
+
+document.querySelectorAll<HTMLAnchorElement>('a').forEach((element) => {
+	element.addEventListener('click', () => {
+		const page = element.href.split('#')[1];
+		console.log(page);
+	});
+});
